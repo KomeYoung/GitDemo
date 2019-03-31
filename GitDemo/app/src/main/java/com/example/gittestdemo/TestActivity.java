d32865ee2e24aa940fd5c9c6d59ae6f4b45632bd
lenovo <605036281@qq.com>
2019-03-31 18:49:26 +0800

    男2号提交

--- GitDemo/app/src/main/java/com/example/gittestdemo/TestActivity.java
+++ GitDemo/app/src/main/java/com/example/gittestdemo/TestActivity.java
@@ -15,6 +15,7 @@ public class TestActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //我的--
 
     }
 }
