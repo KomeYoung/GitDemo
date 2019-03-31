c26e731557731465c1a90fcf3c7a099752667c02
kome <17629052324@163.com>
2019-03-31 18:27:28 +0800

    add  from mac  kome

---
+++ GitDemo/app/src/main/java/com/example/gittestdemo/TestActivity.java
@@ -0,0 +1,20 @@
+package com.example.gittestdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Company    : 上海梅洛体育文化有限公司
+ * Author     : kome
+ * E-mail     : 17629052324@163.com
+ * Date       : 2019/3/31 下午6:24
+ * Description:
+ */
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
